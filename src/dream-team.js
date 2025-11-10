1a4af3c4bee3c2610f4f34ce7dca951007793eb6
roguestone <roguestoneinweb@gmail.com>
2025-11-10 21:33:09 +0300

    feat: implement dream-team.js problem

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const { NotImplementedError } = require("../lib");
 
 /**
  * Create name of dream team based on the names of its members
@@ -13,11 +13,17 @@ const { NotImplementedError } = require('../lib');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function createDreamTeam(members) {
+ if (!Array.isArray(members)) return false;
+
+ return members
+  .filter((member) => typeof member === "string")
+  .map((name) => name.trim()[0].toUpperCase())
+  .filter((letter) => letter)
+  .sort()
+  .join("");
 }
 
 module.exports = {
-  createDreamTeam
+ createDreamTeam,
 };

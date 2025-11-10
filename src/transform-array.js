24225e229a91509f6eddd265febdfff38049392a
roguestone <roguestoneinweb@gmail.com>
2025-11-10 19:27:12 +0300

    feat: implement transform-array.js problem

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const { NotImplementedError } = require("../lib");
 
 /**
  * Create transformed array based on the control sequences that original
@@ -13,11 +13,41 @@ const { NotImplementedError } = require('../lib');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  *
  */
-function transform(/* arr */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function transform(arr) {
+ const result = [];
+ const discardNextIndexes = new Set();
+
+ for (let i = 0; i < arr.length; i++) {
+  const currentItem = arr[i];
+
+  if (discardNextIndexes.has(i)) {
+   continue;
+  }
+
+  if (currentItem === "--discard-next") {
+   if (i + 1 < arr.length) {
+    discardNextIndexes.add(i + 1);
+   }
+  } else if (currentItem === "--discard-prev") {
+   if (i - 1 >= 0 && !discardNextIndexes.has(i - 1)) {
+    result.pop();
+   }
+  } else if (currentItem === "--double-next") {
+   if (i + 1 < arr.length && !discardNextIndexes.has(i + 1)) {
+    result.push(arr[i + 1]);
+   }
+  } else if (currentItem === "--double-prev") {
+   if (i - 1 >= 0 && !discardNextIndexes.has(i - 1)) {
+    result.push(arr[i - 1]);
+   }
+  } else {
+   result.push(currentItem);
+  }
+ }
+
+ return result;
 }
 
 module.exports = {
-  transform
+ transform,
 };

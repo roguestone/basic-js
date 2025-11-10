013e48021a11aff0fe18c061c8657ed05651ec87
roguestone <roguestoneinweb@gmail.com>
2025-11-10 13:26:08 +0300

    feat: implement carbon-dating problem

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const { NotImplementedError } = require("../lib");
 
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
@@ -17,11 +17,20 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
+ if (typeof sampleActivity !== "string") {
+  return false;
+ }
+
+ if (sampleActivity < MODERN_ACTIVITY && sampleActivity > 0) {
+  return Math.ceil(
+   (Math.log(MODERN_ACTIVITY / sampleActivity) / Math.log(2)) * HALF_LIFE_PERIOD
+  );
+ } else {
+  return false;
+ }
 }
 
 module.exports = {
-  dateSample
+ dateSample,
 };

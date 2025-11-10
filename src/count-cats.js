e84d8c9832bbcc284bc3bc354a2060d895d29dca
roguestone <roguestoneinweb@gmail.com>
2025-11-10 13:48:21 +0300

    feat: complete count-cats problem

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const { NotImplementedError } = require("../lib");
 
 /**
  * Given matrix where you have to find cats by ears "^^"
@@ -14,11 +14,26 @@ const { NotImplementedError } = require('../lib');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function countCats(matrix) {
+ if (!Array.isArray(matrix)) {
+  return 0;
+ }
+
+ let catCount = 0;
+
+ for (let row of matrix) {
+  if (Array.isArray(row)) {
+   for (let element of row) {
+    if (element === "^^") {
+     catCount++;
+    }
+   }
+  }
+ }
+
+ return catCount;
 }
 
 module.exports = {
-  countCats
+ countCats,
 };

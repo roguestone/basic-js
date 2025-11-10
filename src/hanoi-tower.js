e6b9d9a3bf142462df545ae3413dd0fbaac6b934
roguestone <roguestoneinweb@gmail.com>
2025-11-10 21:57:39 +0300

    feat: implement hanoi-tower.js problem

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const { NotImplementedError } = require("../lib");
 
 /**
  * Calculate turns number and time (in seconds) required
@@ -14,11 +14,19 @@ const { NotImplementedError } = require('../lib');
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function calculateHanoi(disksNumber, turnsSpeed) {
+ const turns = Math.pow(2, disksNumber) - 1;
+
+ const turnsPerSecond = turnsSpeed / 3600;
+
+ const seconds = Math.floor(turns / turnsPerSecond);
+
+ return {
+  turns: turns,
+  seconds: seconds,
+ };
 }
 
 module.exports = {
-  calculateHanoi
+ calculateHanoi,
 };

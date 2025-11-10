31a664d6155d8129eb9628432dcfcad9430453ad
roguestone <roguestoneinweb@gmail.com>
2025-11-10 18:56:55 +0300

    feat: implement sum-digits.js problem

--- src/sum-digits.js
+++ src/sum-digits.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const { NotImplementedError } = require("../lib");
 
 /**
  * Given a number, replace this number with
@@ -12,11 +12,17 @@ const { NotImplementedError } = require('../lib');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function getSumOfDigits(n) {
+ let num = n;
+
+ while (num >= 10) {
+  const digits = num.toString().split("").map(Number);
+  num = digits.reduce((sum, digit) => sum + digit, 0);
+ }
+
+ return num;
 }
 
 module.exports = {
-  getSumOfDigits
+ getSumOfDigits,
 };
